da1596461381cb34e2cbd1a7cc9e3306011344a9
adeo8 <adeo88@gmail.com>
2021-08-30 16:14:37 -0700

    Initial Scafolding

---
+++ SerialViewer-Plus/SerialViewer-Plus/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using ReactiveUI;
+using SerialViewer_Plus.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SerialViewer_Plus
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ReactiveWindow<TerminalViewModel>
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SerialViewer-Plus/SerialViewer-Plus/ViewModels/TerminalViewModel.cs
@@ -0,0 +1,25 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerialViewer_Plus.ViewModels
+{
+    public class TerminalViewModel : ReactiveObject, IActivatableViewModel
+    {
+        public ViewModelActivator Activator { get; } = new();
+
+        public TerminalViewModel()
+        {
+
+            this.WhenActivated((CompositeDisposable registration) =>
+            {
+
+            });
+
+        }
+    }
+}
